162323e63b40aee654d573b685da3a39ef8e7a6c
Vaishnavi Tompe <90953780+VaishnaviTompe@users.noreply.github.com>
2024-04-13 09:49:40 +0530

    Update Server.js

--- Server.js
+++ Server.js
@@ -92,9 +92,10 @@ mongoose.connection.on("connected", () => {
   console.log("DB connected");
 });
 
-app.use(cors({
-  origin: 'https://gatefe.azurewebsites.net'
-}));
+// app.use(cors({
+//   origin: 'https://gatefe.azurewebsites.net'
+// }));
+app.use(cors());
 app.use(bodyParser.json());
 
 app.use(require("./routes/user_route"));
